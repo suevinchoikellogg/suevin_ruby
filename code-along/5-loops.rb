c0ef8a6e43ad3819def37fb511a488a8bb971eba
suevin choi <suevin.choi@kellogg.northwestern.edu>
2024-01-25 21:01:59 +0000

    Ruby lab 4,5 completed

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -7,5 +7,47 @@
 #   puts "tacos!"
 # end
 
+
+####### (1) 정석대로 짜는 Loop #######
+
 # Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+
+# Set the index to 0 
+index = 0
+
+# Start the loop
+loop do 
+
+    # if the index is outside the range of the array, stop looping
+    if index == tacos.length
+        break
+    end
+
+    # put the taco at the index in memory
+    taco = tacos[index]
+
+    # write it to screen
+    puts "#{taco} tacos"
+    
+    # increment the index
+    index = index + 1
+
+end
+
+
+
+####### (2) 짧게 짜는 Loop #######
+
+tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+for taco in tacos
+    puts "#{taco} tacos"
+end
+
+
+
+### Inefficient way to do the work
+# puts "#{tacos[0]} tacos"
+# puts "#{tacos[1]} tacos"
+# puts "#{tacos[2]} tacos"
+# puts "#{tacos[3]} tacos"
--- labs/5-loops.rb
+++ labs/5-loops.rb
@@ -6,8 +6,6 @@
 # Build a deck of cards. Given the following arrays, use a loop
 # (or two... hint, hint) to write out every combination to the
 # screen.
-ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
-suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 
 # Sample output:
 # 2 of Clubs
@@ -20,3 +18,28 @@ suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 # CHALLENGE
 # Deal a poker hand. Shuffle the deck and "deal" (i.e. display) a 5 card hand (i.e. 5 cards from the deck).
 # You will want to look at the documentation for Arrays: https://ruby-doc.org/core-2.7.0/Array.html
+
+
+
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+
+# Loop through each rank and each suit to generate all combinations
+rank_index = 0
+suit_index = 0
+
+loop do
+  rank = ranks[rank_index]
+  suit = suits[suit_index]
+
+  puts "#{rank} of #{suit}"
+
+  suit_index += 1
+  if suit_index == suits.length
+    suit_index = 0
+    rank_index += 1
+  end
+
+  break if rank_index == ranks.length
+end
+
